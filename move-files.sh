00db8e2f11678f657d42e2e84aebfaae7d9c0585
Joe Love <git@joelove.uk>
2018-11-26 18:34:16 +0000

    update move files script

--- move-files.sh
+++ move-files.sh
@@ -1,4 +1,4 @@
-# Replace relative references to /src directory in dev server, package.json and webpack configs
+echo 'Update references to /src folder...'
 SRC_PATTERN='s/\.\.\/src/\.\/src/';
 
 sed -i "$SRC_PATTERN" dev-server.js
@@ -7,43 +7,40 @@ sed -i "$SRC_PATTERN" webpack.config.js
 sed -i "$SRC_PATTERN" webpack.production.config.js
 sed -i "$SRC_PATTERN" webpack.test.config.js
 
-# Move the sass folder and remove Ira.Website.Common
+echo 'Moving SASS...'
 mv src/Ira.Website.Common/sass src/Ira.Website.Portals.Employee.React
 rm -r src/Ira.Website.Common
 
-# Replace references to Ira.Website.Common/sass in in webpack configs
+echo 'Updating webpack config...'
 SASS_PATTERN='s/Ira\.Website\.Common\/sass/Ira.Website.Portals.Employee.React\/sass/';
 
 sed -i "$SASS_PATTERN" webpack.config.js
 sed -i "$SASS_PATTERN" webpack.test.config.js
 sed -i "$SASS_PATTERN" webpack.production.config.js
 
-# Move common scripts to Employee.React folder
+echo 'Combining scripts folders...'
 rsync -avh --progress --ignore-existing src/Ira.Website.Portals.Common/scripts/ira-portals-common/ src/Ira.Website.Portals.Employee.React/scripts/
 
+echo 'Merging index files...'
 cat \
   src/Ira.Website.Portals.Common/scripts/ira-portals-common/components/grid/fields/index.js \
-  src/Ira.Website.Portals.Employee.React/scripts/components/grid/fields/index.js \
-  > src/Ira.Website.Portals.Employee.React/scripts/components/grid/fields/index.js
+  >> src/Ira.Website.Portals.Employee.React/scripts/components/grid/fields/index.js
 
 cat \
   src/Ira.Website.Portals.Common/scripts/ira-portals-common/components/index.js \
-  src/Ira.Website.Portals.Employee.React/scripts/components/index.js \
-  > src/Ira.Website.Portals.Employee.React/scripts/components/index.js
+  >> src/Ira.Website.Portals.Employee.React/scripts/components/index.js
 
-cat \
-  src/Ira.Website.Portals.Common/scripts/ira-portals-common/components/panels/index.js \
-  src/Ira.Website.Portals.Employee.React/scripts/components/panels/index.js \
-  > src/Ira.Website.Portals.Employee.React/scripts/components/panels/index.js
+cat src/Ira.Website.Portals.Common/scripts/ira-portals-common/components/panels/index.js \
+  >> src/Ira.Website.Portals.Employee.React/scripts/components/panels/index.js
 
 cat \
   src/Ira.Website.Portals.Common/scripts/ira-portals-common/containers/index.js \
-  src/Ira.Website.Portals.Employee.React/scripts/containers/index.js \
-  > src/Ira.Website.Portals.Employee.React/scripts/containers/index.js
+  >> src/Ira.Website.Portals.Employee.React/scripts/containers/index.js
 
+echo 'Removing common folder...'
 rm -r src/Ira.Website.Portals.Common
 
-# Replace references common folder webpack aliases
+echo 'Updating webpack config...'
 COMMON_PATTERN='s/Ira\.Website\.Portals\.Common\/scripts\/ira-portals-common/Ira.Website.Portals.Employee.React\/scripts/';
 
 sed -i "$COMMON_PATTERN" webpack.config.js
